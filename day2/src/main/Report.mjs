1e362766500c027f8b12ded62dbb3b628279fc95
Satvik Sharma <satvik@easyagile.com>
2024-12-05 17:06:46 +1100

    Remove input files

--- day2/src/main/Report.mjs
+++ day2/src/main/Report.mjs
@@ -57,57 +57,38 @@ export default class Report {
   }
 
   meetsSafetyStandardsV2() {
-    let thePreviousLevel = this.#reportLevelsArray[0];
-    let levelsAreAscending = new Boolean(
-      this.#reportLevelsArray[1] > thePreviousLevel
-    );
-    let badLevelIndexes = [];
+    if (!this.meetsSafetyStandards()) {
+        console.log("Original: ", this.toString())
+        for (let index = 0; index < this.#reportLevelsArray.length; index++) {
+            const filteredLine = this.#reportLevelsArray.filter((_, i) => i !== index).join(" ");
+            const subReport = new Report(filteredLine);
 
-    for (let index = 1; index < this.#reportLevelsArray.length; index++) {
-      const current = this.#reportLevelsArray[index];
-
-      if (current > thePreviousLevel) {
-        if (current - thePreviousLevel > 3) {
-          badLevelIndexes.push(index);
-        } else if (levelsAreAscending.valueOf() === false) {
-          badLevelIndexes.push(index);
+            console.log({ index, filteredLine, passes: subReport.meetsSafetyStandards() })
+            if (subReport.meetsSafetyStandards()) {
+                return true;
+            }
         }
-      } else if (current < thePreviousLevel) {
-        if (thePreviousLevel - current > 3) {
-          badLevelIndexes.push(index);
-        } else if (levelsAreAscending.valueOf() === true) {
-          badLevelIndexes.push(index);
-        }
-      } else {
-        badLevelIndexes.push(index);
-      }
 
-      thePreviousLevel = current;
-    }
-
-    if (badLevelIndexes.length === 1) {
-      if (this.#subReportCheck === true) {
         return false;
-      }
-
-      const numberOfValidSubReports = badLevelIndexes
-        .map((badIndex) => {
-          const newReportLine = this.#reportLevelsArray
-            .filter((_, index) => index !== badIndex)
-            .join(" ");
-          return new Report(newReportLine, true);
-        })
-        .filter((subReport) => subReport.meetsSafetyStandards()).length;
-
-      if (numberOfValidSubReports >= 1) {
-        return true;
-      } else {
-        return false;
-      }
     } else {
-      return true;
+        return true;
     }
-  }
+
+    //   const numberOfValidSubReports = badLevelIndexes
+    //     .map((badIndex) => {
+    //       const newReportLine = this.#reportLevelsArray
+    //         .filter((_, index) => index !== badIndex)
+    //         .join(" ");
+    //       return new Report(newReportLine, true);
+    //     })
+    //     .filter((subReport) => subReport.meetsSafetyStandards()).length;
+
+    //   if (numberOfValidSubReports >= 1) {
+    //     return true;
+    //   } else {
+    //     return false;
+    //   }
+    }
 
   toString() {
     return this.#reportLevelsArray.join(" ");
